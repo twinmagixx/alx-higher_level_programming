6bcd92356535037ff875122bfce47ef4c8963b76
twinmagixx <andilendwandwe1902@gmail.com>
2023-10-03 13:23:07 +0200

    test commit

---
+++ 0x09-python-everything_is_object/tests/19-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Sazi A. Ndwandwe
+
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
